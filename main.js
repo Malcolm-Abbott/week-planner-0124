835b6e4e38f6a5ba5f6c7c516aeef8dba89f5c5a
Malcolm Abbott <mjacoding@gmail.com>
2024-02-24 00:29:24 +0000

    Data model + localStorage functionality

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
+var data = {
+  entries: [],
+};
+window.addEventListener('beforeunload', function () {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('week-planner-storage', dataJSON);
+});
+var previousDataJSON = localStorage.getItem('week-planner-storage');
+if (previousDataJSON !== null) data = JSON.parse(previousDataJSON);
 var $add = document.querySelector('.add-event');
 var $dialog = document.querySelector('dialog');
+var $form = document.querySelector('form');
+var $daysDD = document.querySelector('#days-drop-down');
+var $times = document.querySelector('#times');
+var $info = document.querySelector('#info');
+$form === null || $form === void 0
+  ? void 0
+  : $form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      var daysDD = $daysDD.value;
+      var times = $times.value;
+      var info = $info.value;
+      var values = {
+        daysDD: daysDD,
+        times: times,
+        info: info,
+      };
+      data.entries.push(values);
+      console.log('data.entries:', data.entries);
+    });
 $add === null || $add === void 0
   ? void 0
   : $add.addEventListener('click', function () {
       $dialog.showModal();
     });
+var $cancel = document.querySelector('.cancel');
+var $confirm = document.querySelector('.confirm');
+$cancel === null || $cancel === void 0
+  ? void 0
+  : $cancel.addEventListener('click', function () {
+      $dialog.close();
+    });
+$confirm === null || $confirm === void 0
+  ? void 0
+  : $confirm.addEventListener('click', function () {
+      $dialog.close();
+    });
+$form === null || $form === void 0
+  ? void 0
+  : $form.addEventListener('beforeunload', function (event) {
+      event.preventDefault();
+    });
+// const $days = document.querySelector('#days') as HTMLSelectElement;
+// const $dayOptions = $days.options as HTMLOptionsCollection;
+// for (let i = 0; i < $dayOptions.length; i++) {
+//   let option = $dayOptions[i];
+//   console.log('option.value:', option.value);
+// }
--- main.ts
+++ main.ts
@@ -1,6 +1,74 @@
+interface Values {
+  daysDD: string;
+  times: string;
+  info: string;
+}
+
+interface Data {
+  entries: Values[];
+}
+
+let data: Data = {
+  entries: [],
+};
+
+window.addEventListener('beforeunload', () => {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('week-planner-storage', dataJSON);
+});
+
+const previousDataJSON = localStorage.getItem('week-planner-storage');
+
+if (previousDataJSON !== null) data = JSON.parse(previousDataJSON);
+
 const $add = document.querySelector('.add-event') as HTMLButtonElement;
 const $dialog = document.querySelector('dialog') as HTMLDialogElement;
+const $form = document.querySelector('form') as HTMLFormElement;
+const $daysDD = document.querySelector('#days-drop-down') as HTMLSelectElement;
+const $times = document.querySelector('#times') as HTMLSelectElement;
+const $info = document.querySelector('#info') as HTMLTextAreaElement;
+
+$form?.addEventListener('submit', (event: Event): void => {
+  event.preventDefault();
+
+  const daysDD = $daysDD.value;
+  const times = $times.value;
+  const info = $info.value;
+
+  const values: Values = {
+    daysDD,
+    times,
+    info,
+  };
+
+  data.entries.push(values);
+  console.log('data.entries:', data.entries);
+});
 
 $add?.addEventListener('click', (): void => {
   $dialog.showModal();
 });
+
+const $cancel = document.querySelector('.cancel') as HTMLButtonElement;
+const $confirm = document.querySelector('.confirm') as HTMLButtonElement;
+
+$cancel?.addEventListener('click', (): void => {
+  $dialog.close();
+});
+
+$confirm?.addEventListener('click', (): void => {
+  $dialog.close();
+});
+
+$form?.addEventListener('beforeunload', (event: Event): void => {
+  event.preventDefault();
+});
+
+// const $days = document.querySelector('#days') as HTMLSelectElement;
+
+// const $dayOptions = $days.options as HTMLOptionsCollection;
+
+// for (let i = 0; i < $dayOptions.length; i++) {
+//   let option = $dayOptions[i];
+//   console.log('option.value:', option.value);
+// }
